a24b95cb9c72e30bfbf2c4e1a8bc599c5732001d
lee14334 <lsiyeun@gmail.com>
2024-09-26 19:53:41 -0400

    Add error handlers

--- example_4.py
+++ example_4.py
@@ -36,3 +36,12 @@ def index():
             
         return redirect(url_for('index'))
     return render_template('index.html', form=form, name=session.get('name'), email=session.get('email'))
+
+# Error handlers
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
