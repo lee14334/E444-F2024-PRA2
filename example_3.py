09861b0103236a3724c4a8a56b114b2336b07852
lee14334 <lsiyeun@gmail.com>
2024-09-26 19:07:04 -0400

    Reproduce up till example 4.7

--- example_3.py
+++ example_3.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
+
 app = Flask(__name__)
 
 bootstrap = Bootstrap(app)
